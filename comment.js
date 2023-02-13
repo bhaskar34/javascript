0839f254790a78f008d483885862286add4aecb8
bhaskar34 <58424334+bhaskar34@users.noreply.github.com>
2023-02-13 20:08:23 +0530

    Add files via upload

---
+++ comment.js
@@ -0,0 +1,18 @@
+// I
+// am 
+// Starting my new journey in JavaScript
+// This is a single lined comment
+
+/* 
+    I
+    am
+    Starting my new journey in JavaScript
+*/
+
+/*
+    this
+    is
+    a 
+    Multi lined 
+    comment
+*/
