66c2fa09650502bb69b5b10f8a5ab56a8d77582d
bhaskar34 <58424334+bhaskar34@users.noreply.github.com>
2023-02-17 11:52:36 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+var clothes=[];
+clothes.push("towel");      // 1st value
+clothes.push("shirt");      // 2nd value
+clothes.push("dhoti");      // 3rd value
+clothes.push("kurtha");     // 4th value
+clothes.push("payjama");    // 5th value
+clothes.pop();              // remove 5th value
+console.log("clothes[2]");  // display 3th value
