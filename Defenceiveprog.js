f0c6e890142d19111d7f0864c13b237a43986a61
bhaskar34 <58424334+bhaskar34@users.noreply.github.com>
2023-02-17 19:37:07 +0530

    Add files via upload

---
+++ Defenceiveprog.js
@@ -0,0 +1,25 @@
+/*
+    Defensive programming is an approach where in the programmer 
+    assumes he is capable of mistakes, and therefore can apply 
+    the proper practices to produce higher-quality code.
+*/
+
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
+    if(condition1 && condition2) { //if both condition matches
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //check if the character at this i position in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        //if the requirements don't match
+        console.log("Please pass correct arguments to the function")
+    }
+}
+letterFinder([],[])
+letterFinder("cat","c")
