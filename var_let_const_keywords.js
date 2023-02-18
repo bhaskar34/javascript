b169266b6141ae55061b0e46a015c8c82d519b07
bhaskar34 <58424334+bhaskar34@users.noreply.github.com>
2023-02-18 14:17:05 +0530

    Add files via upload

---
+++ var_let_const_keywords.js
@@ -0,0 +1,24 @@
+// Var keyword
+
+console.log(user);              // variable used before declaring
+var user="user1";
+
+var user="user1";               //
+var user="user2";               // same variable name can be used many times     
+var user="user3";               //
+console.log(user);              // output: user3 ; // The last declared variable will be displayed
+
+// Let keyword
+
+// let keyword cannot be used multiple times like var for same variable name
+
+// let keyword variable cannot be used before declaring
+let user="user1";
+console.log(user);
+
+
+// Const keyword
+// it is a fixed variable once initialized cannot be changed
+
+const user="user1";
+console.log(user);
