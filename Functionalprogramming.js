08255924f8a2b8b54b924812d801f87fcda05792
bhaskar34 <58424334+bhaskar34@users.noreply.github.com>
2023-02-18 12:16:16 +0530

    Add files via upload

---
+++ Functionalprogramming.js
@@ -0,0 +1,11 @@
+//  This is the format of a functional programming
+
+
+var company="Tata";                                                     // data variables
+var model="Nano";
+
+function car(company,model){                                            // function 
+    console.log("The company is ",company," and the model is ",model);
+}
+
+car(company,model);                                                     // function call
